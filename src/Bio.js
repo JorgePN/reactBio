9c638629b9cee7b89c586dfe0ba1dcbaac8cad76
Jorge Naranjo <jpna188@gmail.com>
2019-02-25 14:01:35 -0500

    onePointAdded

--- src/Bio.js
+++ src/Bio.js
@@ -5,7 +5,7 @@ class Bio extends Component {
     return (
       <div className="Bio">
             <h2> My name is Jorge and I am 31 years old </h2><br />
-            <p> I like camping, reading, and taking long walks. </p>
+            <p> I like camping, reading, and taking long walks.. </p>
             <img src="https://ichef.bbci.co.uk/news/660/cpsprodpb/B7B0/production/_101542074_gettyimages-956391468.jpg" alt="forest" />
       </div>
     )
